1986f89c7a2095e7c668502efcff537fba739272
Ubuntu <ubuntu@ip-172-31-32-158.ap-northeast-1.compute.internal>
2025-09-11 13:42:21 +0000

    9/11 complete

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,10 +2,14 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @user = @book.user
+    @book_new = Book.new
   end
 
   def index
     @books = Book.all
+    @book = Book.new
+    @user = current_user
   end
 
   def create
@@ -21,10 +25,16 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+    unless @book.user == current_user
+      redirect_to books_path
+    end
   end
 
   def update
     @book = Book.find(params[:id])
+    unless @book.user == current_user
+      redirect_to books_path
+    end
     if @book.update(book_params)
       redirect_to book_path(@book), notice: "You have updated book successfully."
     else
@@ -32,15 +42,15 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
   end
 end
