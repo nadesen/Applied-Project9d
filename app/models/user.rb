291b57f4b091f11365a029839db9bee57e362efc
Ubuntu <ubuntu@ip-172-31-32-158.ap-northeast-1.compute.internal>
2025-09-12 06:24:28 +0000

    9/12 complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
 
   has_many :books, dependent: :destroy
   has_one_attached :profile_image
+  has_many :book_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 
   validates :name, presence: true, uniqueness: true, length: { minimum: 2, maximum: 20 }
   validates :introduction, length: { maximum: 50 }
@@ -13,6 +15,10 @@ class User < ApplicationRecord
   
   
   def get_profile_image
-    (profile_image.attached?) ? profile_image : 'no_image.jpg'
+    if profile_image.attached?
+      profile_image
+    else
+      'no_image.jpg'
+    end
   end
 end
