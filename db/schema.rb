cc2bd40ec8e457c60cb88687c21b3a6941c6dead
nadesen <mononokehimehafukai@gmail.com>
2025-09-27 12:14:59 +0000

    9/27_Applied-Project7d

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_09_13_055016) do
+ActiveRecord::Schema.define(version: 2025_09_27_113919) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2025_09_13_055016) do
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "star"
   end
 
   create_table "favorites", force: :cascade do |t|
